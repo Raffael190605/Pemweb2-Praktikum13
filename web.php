6778c7065a4f83dd6c1d8ff2dd32884b99a59f0f
Raffael190605 <152363550+Raffael190605@users.noreply.github.com>
2024-06-12 10:40:04 +0700

    Add files via upload

---
+++ web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentController;
+
+
+
+/**
+ * Method HTTP:
+ * 1. Get = Menampilkan halaman
+ * 2. Post = mengirim data
+ * 3. Put = meng-update data
+ * 4. Delete = mengapus data
+ */
+
+// route untuk menampilkan teks
+Route::get('/salam/{nama}', function ($nama) {
+    return "Assalamualaikum $nama";
+});
+
+// route untuk menamplilkan student
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('admin/student', [StudentController::class, 'index']);
+
+Route::get('admin/student/create', [StudentController::class, 'create']);
+
+// Route untuk mengirim data student
+Route::post('admin/student/store', [StudentController::class, 'store']);
+
+    Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
